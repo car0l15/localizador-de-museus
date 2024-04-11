9cbc7e68ff976e4d547bf5cae95b79c7c7431f63
car0l15 <carol.nepomuceno95@gmail.com>
2024-04-11 11:31:03 -0300

    feat: criando exceção caso um museu próximo não seja encontrado e retornando um museu casos seja encontrado

---
+++ src/main/java/com/betrybe/museumfinder/exception/MuseumNotFoundException.java
@@ -0,0 +1,12 @@
+package com.betrybe.museumfinder.exception;
+
+/**
+ * The type Museum not found exception.
+ */
+public class MuseumNotFoundException extends RuntimeException {
+
+  public MuseumNotFoundException() {
+    super("Nenhum museu próximo foi localizado");
+  }
+
+}
